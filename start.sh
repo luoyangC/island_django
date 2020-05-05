9b62bcd36ed05dc3842f181c2457a011d5ce44a5
luoyangC <luoyang15261829198@gmail.com>
2020-05-05 16:06:37 +0800

    fix 配置文件

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py collectstatic --noinput &&
 python manage.py migrate &&
-gunicorn luoyangc.wsgi:application -c gunicorn.conf
+gunicorn config.wsgi:application -c gunicorn.conf
